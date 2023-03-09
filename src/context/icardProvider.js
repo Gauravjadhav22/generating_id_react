da05e074d9709fcd35ab194d0b911f2a9defab10
gaurav jadhav <gauravjadhav7512@gmail.com>
2023-03-10 00:14:25 +0530

    pdf grid

--- src/context/icardProvider.js
+++ src/context/icardProvider.js
@@ -5,40 +5,64 @@ const StudentContext = createContext()
 
 
 export const StudentProvider = ({ children }) => {
-    const [studentData, setStudentData] = useState({})
-    const [studentsData, setStudentsData] = useState([])
- 
-    const addStudents =  useCallback(
-      (newState ) =>   {
-        console.log(newState);
-        setStudentsData([...newState])
+  const [studentData, setStudentData] = useState({})
+  const [studentsData, setStudentsData] = useState([])
+
+  const addStudents = useCallback(
+    (newState) => {
+      console.log(newState);
+      console.log(Math.round((newState.length / 4) + 1));
+      let length = Math.round((newState.length / 4) + 1)
+      let count = 0;
+      let bigArr = []
+      for (let index = 0; index < length; index++) {
+        let arr = []
+        if (!newState[count]) {
+          break
+        }
+        for (let i = 0; i < 6; i++) {
+
+          if (!newState[count]) {
+            break
+          }
+          arr.push(newState[count])
+          console.log();
+          count++;
+
+        }
+        bigArr.push(arr)
+
+      }
+      console.log(bigArr);
+
+      setStudentsData([...bigArr])
 
     },
-      [studentsData,setStudentsData],
-    )
-    const addStudent =  useCallback(
-      (newState ) =>   {
-        console.log(newState);
-        setStudentData({...newState})
+    [studentsData, setStudentsData],
+  )
+  const addStudent = useCallback(
+    (newState) => {
+      console.log(newState);
+      setStudentData({ ...newState })
 
     },
-      [studentData,setStudentData],
-    )
-  
-    const getStudents = useCallback(
-        () => ({setStudentsData, ...studentsData}),
-        [studentsData, setStudentsData],
-      )
-    const getStudent = useCallback(
-        () => ({setStudentData, ...studentData}),
-        [studentData, setStudentData],
-      )
-    
+    [studentData, setStudentData],
+  )
 
-    return (
-        <StudentContext.Provider value={{getStudents,getStudent,addStudent, studentData, addStudents ,studentsData}}>
-            {children}
-        </StudentContext.Provider>)
+  const getStudents = useCallback(
+    () => ({ setStudentsData, ...studentsData }),
+    [studentsData, setStudentsData],
+  )
+  const getStudent = useCallback(
+    () => ({ setStudentData, ...studentData }),
+    [studentData, setStudentData],
+  )
+
+
+  return (
+    <StudentContext.Provider value={{ getStudents, getStudent, addStudent, studentData, addStudents, studentsData }}>
+      {children}
+    </StudentContext.Provider>)
 }
 
 export default StudentContext
--- src/pages/Icards.js
+++ src/pages/Icards.js
@@ -16,8 +16,9 @@ import { Page, Text, Image, Document, StyleSheet, View } from "@react-pdf/render
 
 const Icards = () => {
     const { studentsData, getStudents } = useContext(StudentContext)
-
-
+    console.log(studentsData);
+    // const [frontData, setFrontData] = useState([])
+    let frontData = []
 
     const PdfFile = () => {
 
@@ -26,98 +27,93 @@ const Icards = () => {
 
         return (
             <Document >
-                {
-                    studentsData?.map((studentData, index) => {
+                {studentsData.map((itm, index) => <Page key={index} style={{ textTransform: "capitalize", display: "flex", flexDirection: "row", flexWrap: "wrap" }} >
 
-                        return <Page style={{ textTransform: "capitalize", }} >
+                    {itm.map((studentData, index) => {
+                        return <View style={{ border: "1px solid black", backgroundColor: "purple", height: "25%", width: "45%", boxSizing: "border-box", display: "flex", flexDirection: "column", justifyContent: "flex-start", borderRadius: "25px", marginTop: "5px", margin: "12px" }} key={index + 'key'}>
+
+                            <View style={{ backgroundColor: "#BF55EC", borderRadius: "25px", display: "flex", justifyContent: "center", alignItems: "center", color: "white", flexDirection: "row", padding: "1px", paddingLeft: "2px" }}>
+                                <View>
+
+                                    <Image alt="img.jpg" src={logo} style={{ width: "55px" }} />
+                                </View>
+                                <View style={{ marginHorizontal: "15px", color: 'white', justifyContent: "center", alignItems: "center", }}>
+
+                                    <Text style={{ marginTop: "5px", textAlign: "center", fontSize: "12px" }} >mahatma gandhi mission's</Text>
+                                    <Text style={{ marginTop: "5px", textTransform: "uppercase", fontSize: "10px" }} >college of computer science & it</Text>
+                                    <Text style={{ marginTop: "5px", marginBottom: "8px", fontWeight: "bold", fontSize: "5px", alignSelf: "flex-start" }}>near airport nanded-431605 tel-45645*** web:https://www.mgmccsit.ac.in/</Text>
+                                </View>
+
+                            </View>
+                            <View style={{ padding: "10px", backgroundColor: "white", textAlign: "left", display: "flex", flexDirection: "column", alignContent: "start", alignItems: "left", margin: "5px", borderRadius: "10px" }}>
+                                <View style={{ display: 'flex', justifyContent: "between", flexDirection: "row", marginBottom: "15px" }}>
+                                    <View  >
+                                        <Text style={{ marginTop: "10px", fontSize: "8px" }} fixed>Name: {studentData.fullName}</Text>
+                                        <Text style={{ marginTop: "10px", fontSize: "8px" }} fixed>Age: {studentData.age}</Text>
+                                        <Text style={{ marginTop: "10px", fontSize: "8px" }} fixed>Address: {studentData.address}</Text>
+                                        <Text style={{ marginTop: "10px", fontSize: "8px" }} fixed>Phone: {studentData.phone}</Text>
+
+                                    </View>
+                                    <View style={{ width: "65px", height: "70px", alignSelf: "flex-end", marginLeft: "35px", overflow: 'hidden', display: "flex", justifyContent: "center" }}>
+                                        <Image alt="img.jpg" src={studentData.userPic} style={{ width: "63px", height: "70px", border: "5px solid purple", borderRadius: "5px", backgroundColor: "purple", padding: "1px" }} />
+                                        <Image alt="img.jpg" src={sign} style={{ width: "55px", height: "32px", position: 'absolute', top: "55%", left: "1%" }} />
+                                    </View>
+                                </View>
 
 
-
-
-                            <View style={{ backgroundColor: "purple", height: "400px", boxSizing: "border-box", display: "flex", flexDirection: "column", justifyContent: "flex-start", border: "1px solid black", borderRadius: "25px", marginHorizontal: "25px", marginTop: "5px" }} key={index + 'key'}>
-
-                                <View style={{ backgroundColor: "#BF55EC", borderRadius: "25px", display: "flex", justifyContent: "center", alignItems: "center", color: "white", flexDirection: "row", padding: "1px", paddingLeft: "2px" }}>
+                                <View style={{ display: "flex", justifyContent: "space-between", textAlign: "left", alignItems: "center", flexDirection: "row" }}>
                                     <View>
 
-                                        <Image alt="img.jpg" src={logo} style={{ width: "93px" }} />
+                                        <Text style={{ margin: "10px", marginHorizontal: "1", fontSize: "8px" }} fixed>BloodGrp: {studentData.bloodgrp}</Text>
+                                        <Text style={{ margin: "10px", marginHorizontal: "1", fontSize: "8px" }} fixed>Dob: {studentData.dob}</Text>
                                     </View>
-                                    <View style={{ marginHorizontal: "20px", color: 'white', justifyContent: "center", alignItems: "center" }}>
+                                    <View>
 
-                                        <Text style={{ marginTop: "8px", textAlign: "center", fontSize: "18px" }} >mahatma gandhi mission's</Text>
-                                        <Text style={{ marginTop: "8px", textTransform: "uppercase", fontSize: "22px" }} >college of computer science & it</Text>
-                                        <Text style={{ marginTop: "8px", marginBottom: "8px", fontWeight: "bold", fontSize: "12px" }}>near airport nanded-431605 tel-45645*** web:https://www.mgmccsit.ac.in/</Text>
+                                        <Text style={{ margin: "10px", marginHorizontal: "1", fontSize: "8px" }} fixed>Email: {studentData.email}</Text>
+                                        <Text style={{ margin: "10px", marginHorizontal: "1", fontSize: "8px" }} fixed>ClassName: {studentData.className}</Text>
                                     </View>
-
                                 </View>
-                                <View style={{ padding: "10px", backgroundColor: "white", textAlign: "left", display: "flex", flexDirection: "column", alignContent: "start", alignItems: "left", margin: "8px", borderRadius: "10px" }}>
-                                    <View style={{ display: 'flex', justifyContent: "between", flexDirection: "row", marginBottom: "15px" }}>
-                                        <View  >
-                                            <Text style={{ margin: "10px", }} fixed>Name: {studentData.fullName}</Text>
-                                            <Text style={{ margin: "10px", }} fixed>Age: {studentData.age}</Text>
-                                            <Text style={{ margin: "10px", }} fixed>Address: {studentData.address}</Text>
-                                            <Text style={{ margin: "10px", }} fixed>Phone: {studentData.phone}</Text>
-
-                                        </View>
-                                        <View style={{ flex: "1", alignSelf: "flex-end", marginLeft: "55px", border: "2px solid purple", padding: "0px", borderRadius: "25px", paddingVertical: "0px", display: "flex", justifyContent: "flex-start", alignItems: 'center', flexDirection: "column", overflow: 'hidden' }}>
-                                            <Image alt="img.jpg" src={studentData.userPic} style={{ width: "183px", height: "158px" }} />
-                                            <Image alt="img.jpg" src={sign} style={{ width: "143px", height: "52px", position: 'absolute', top: "71%" }} />
-                                        </View>
-                                    </View>
-
-
-                                    <View style={{ display: "flex", justifyContent: "space-between", textAlign: "left", alignItems: "center", flexDirection: "row" }}>
-                                        <View>
-
-                                            <Text style={{ margin: "10px", marginHorizontal: "1" }} fixed>BloodGrp: {studentData.bloodgrp}</Text>
-                                            <Text style={{ margin: "10px", marginHorizontal: "1" }} fixed>Dob: {studentData.dob}</Text>
-                                        </View>
-                                        <View>
-
-                                            <Text style={{ margin: "10px", marginHorizontal: "1" }} fixed>Email: {studentData.email}</Text>
-                                            <Text style={{ margin: "10px", marginHorizontal: "1" }} fixed>ClassName: {studentData.className}</Text>
-                                        </View>
-                                    </View>
-
-                                </View>
-
-
 
                             </View>
 
 
-                        </Page>
+
+                        </View>
                     })}
-                {
-                    studentsData?.map((studentData, index) => {
 
-                        return <Page style={{ textTransform: "capitalize", }} >
+                </Page>)}
+                {studentsData.map((itm, index) => <Page key={index} style={{ textTransform: "capitalize", display: "flex", flexDirection: "row", flexWrap: "wrap" }} >
+
+                    {
+                        itm?.map((studentData, index) => {
 
 
-                            <View style={{ height: "400px", display: "flex", flexDirection: "column", border: "1px solid black", borderRadius: "25px", justifyContent: "flex-start", margin: "25px", overflow: "hidden" }} key={index + 'key'}>
 
-                                <View style={{ padding: "15px" }}>
+
+                            return <View style={{ height: "25%", width: "45%", display: "flex", flexDirection: "column", border: "1px solid black", borderRadius: "15px", justifyContent: "flex-start", margin: "12px", overflow: "hidden" }} key={index + 'key'}>
+
+                                <View style={{ padding: "10px" }}>
                                     <View  >
 
-                                        <Text style={{ marginTop: "15", fontWeight: "bold", fontSize: "20px" }}>- the student should possess identity card and must produce whenever demanded.</Text>
+                                        <Text style={{ marginTop: "10px", fontWeight: "bold", fontSize: "10px" }}>- the student should possess identity card and must produce whenever demanded.</Text>
                                     </View>
                                     <View >
-                                        <Text style={{ marginTop: "15", fontWeight: "bold", fontSize: "20px" }}>- if it is lost. the card holder must intimate to the principal and apply for a new card within a week.</Text>
+                                        <Text style={{ marginTop: "10px", fontWeight: "bold", fontSize: "10px" }}>- if it is lost. the card holder must intimate to the principal and apply for a new card within a week.</Text>
                                     </View>
                                     <View >
-                                        <Text style={{ marginTop: "15", fontWeight: "bold", fontSize: "20px" }}>- if this card does not belong to you, please return it to: <i>the principal, MGM'S College of comp. Sci & It, nanded</i></Text>
+                                        <Text style={{ marginTop: "10px", fontWeight: "bold", fontSize: "10px" }}>- if this card does not belong to you, please return it to: <i>the principal, MGM'S College of comp. Sci & It, nanded</i></Text>
                                     </View>
                                 </View>
-                                <Image alt="img.jpg" src={bgImg} style={{ width: "618px", height: "218px", padding: "-32px" }} />
+                                <Image alt="img.jpg" src={bgImg} style={{ width: "618px", height: "118px", padding: "-32px" }} />
 
                                 <Image
-                                    style={{ height: "125px", width: "152px", position: "absolute", top: "50%" }}
+                                    style={{ height: "65px", width: "65px", position: "absolute", top: "50%" }}
                                     src={`${`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=name: ${studentData.fullName} --> ParentContact: ${studentData.parentPhone} --> MotherName: ${studentData.motherName}`}`}
                                 />
                             </View>
 
-                        </Page>
-                    })}
-
+                        })}
+                </Page>)}
             </Document >
         )
     }
@@ -135,7 +131,7 @@ const Icards = () => {
 
 
             {
-                <PDFDownloadLink document={<PdfFile students={studentsData} />} filename="FORM">
+                <PDFDownloadLink document={<PdfFile />} filename="FORM">
                     {({ loading }) => (loading ?
                         <div className='text-3xl text-black flex justify-center mt-52 items-center'>
                             <img src={load} height={150} width={150} />
